047228f0721c5b54ee03a1e4d7eb0b712c6bff86
Haoji Tang <haoji828@yahoo.com>
2016-05-28 01:39:07 -0700

    Added multiplay photon feature
    - first commit with project set up
    - created scene1 with playerA and its camera
    - implemented multiplay using photon

---
+++ Assets/Scripts/RoomMan.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomMan : Photon.MonoBehaviour {
+
+    // if you change this version number, people with old versions cannot play until update
+    public string verNum = "0.1";
+    public string roomName = "room01";
+    public Transform spawnPoint;
+    public GameObject playerPref;
+    public bool isConnected = false;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(verNum);
+        Debug.Log("Starting Connection");
+    }
+
+    public void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, null, null);
+        Debug.Log("Starting Server!");
+    }
+
+    public void OnJoinedRoom()
+    {
+        isConnected = true;
+        spawnPlayer();
+    }
+
+    public void spawnPlayer()
+    {
+        GameObject pl = PhotonNetwork.Instantiate(playerPref.name, spawnPoint.position, spawnPoint.rotation, 0) as GameObject;
+        pl.GetComponent<RigidbodyFPSController>().enabled = true;
+        pl.GetComponent<RigidbodyFPSController>().fpsCam.SetActive(true);
+    }
+
+}
